47a7d930123c1d64d665f6dfa8b2b47aaa3386cf
ariffsulaiman <105694846+ariffsulaiman@users.noreply.github.com>
2023-06-07 14:06:36 +0800

    Add files via upload

---
+++ _next/static/D2mgmZbZ0Go7x7kCT3bY9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/495c255344c060a8.css","static/chunks/pages/index-13dcea2549f59edd.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
